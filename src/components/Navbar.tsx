a943665a83b1b5d7e52263fc0f2c6efc84568d50
Antonio-react <Antonio_pk7@hotmail.com>
2026-09-09 22:46:49 +0200

    optimizar logos a webp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,8 +53,19 @@ export default function Navbar() {
             <nav className={`fixed top-0 left-0 w-full md:left-1/2 md:top-6 md:w-max md:-translate-x-1/2 md:rounded-full md:border md:shadow-xl z-50 flex items-center gap-6 md:gap-7 px-5 md:px-6 py-2 transition-all duration-300 ${visible ? 'translate-y-0' : '-translate-y-full md:-translate-y-[calc(100%+1.5rem)]'} ${menuAbierto ? 'bg-black' : ultimoScroll > 80 ? 'bg-black/40 backdrop-blur-md' : 'bg-transparent'} md:bg-transparent! md:backdrop-blur-lg md:backdrop-saturate-150 md:border-white/8`}>
 
                 <Link to="/" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className="border-none bg-transparent p-0 cursor-pointer relative z-50 shrink-0">
-                    <img alt="217 GYM" src="/217-gym.PNG" className="h-14 md:hidden block hover:scale-110" />
-                    <img alt="217 GYM" src="/217.PNG" className="hidden md:block ml-2 h-10 scale-200 hover:scale-225" style={{ aspectRatio: '1024 / 1024' }} />
+                    <picture className="flex items-center">
+                    {/* Versión Escritorio (≥ 768px): Carga solo en PC */}
+                    <source media="(min-width: 768px)" srcSet="/217.webp" />
+
+                    {/* Versión Móvil (Por defecto): Carga solo en Móvil */}
+                    <img 
+                    src="/217-gym.webp" 
+                    alt="217 GYM" 
+                    width="400"
+                    height="234"
+                    className="h-14 w-auto object-contain md:h-10 md:scale-200 hover:scale-110 md:hover:scale-225 transition-transform duration-300" 
+                    />
+                </picture>
                 </Link>
 
                 <ul className="hidden md:flex gap-8 list-none whitespace-nowrap">
